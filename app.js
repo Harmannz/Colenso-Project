b7143879939ff913c4ed246cf0535f8806ce22b7
Harman Singh <Harman Singh>
2016-03-07 01:08:17 +1300

    Added landing page using express for routing.

---
+++ app.js
@@ -0,0 +1,69 @@
+/*
+  Copyright (c) 2008 - 2016 MongoDB, Inc. <http://mongodb.com>
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    nunjucks = require('nunjucks'),
+    MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
+    
+
+// Set up express
+app = express();
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.use('/static', express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+/*
+ Configure nunjucks to work with express
+ Not using consolidate because I'm waiting on better support for template inheritance with
+ nunjucks via consolidate. See: https://github.com/tj/consolidate.js/pull/224
+*/
+var env = nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+var nunjucksDate = require('nunjucks-date');
+nunjucksDate.setDefaultFormat('MMMM Do YYYY, h:mm:ss a');
+env.addFilter("date", nunjucksDate);
+
+var ITEMS_PER_PAGE = 5;
+
+// Hardcoded USERID for use with the shopping cart portion
+var USERID = "558098a65133816958968d88";
+
+    var router = express.Router();
+
+    // Homepage
+    router.get("/", function(req, res) {
+        "use strict";
+        
+    
+		res.render('index');
+    });
+    
+    // Use the router routes in our application
+    app.use('/', router);
+
+    // Start the server listening
+    var server = app.listen(3000, function() {
+        var port = server.address().port;
+        console.log('Colenso Project listening on port %s.', port);
+    });
