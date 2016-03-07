2db94142d0d39a41c0de9ffa1192cc81ca24f245
Harman Singh <Harman Singh>
2016-03-07 23:58:00 +1300

    Fixed License

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 /*
-  Copyright (c) 2008 - 2016 MongoDB, Inc. <http://mongodb.com>
+	Copyright (c) 2016 Harman Singh
+	
+	Permission is hereby granted, free of charge, to any person, except one that is enrolled
+	in SWEN303: User Interface Design at Victoria University of Wellington,
+	obtaining a copy of this software and associated documentation files (the "Software"), 
+	to deal in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
 */
 
 
@@ -19,8 +27,10 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     nunjucks = require('nunjucks'),
     MongoClient = require('mongodb').MongoClient,
-    assert = require('assert');
-    
+    assert = require('assert'),
+	basex  = require("basex"),
+	log = require("./debug");
+
 
 // Set up express
 app = express();
@@ -56,8 +66,29 @@ env.addFilter("date", nunjucksDate);
     // Explore
     router.get("/explore", function(req, res) {
         "use strict";
-        
-		res.render('index', {isHomePage: false});
+	/*
+		// create session
+		var session = new basex.Session("localhost", 1984, "admin", "admin");
+		basex.debug_mode = false;
+		// create query instance
+		var input = 'for $item in db:list("colenso")return $item'; 
+		
+		var query = session.query(input);
+
+		query.results(log.print);
+
+		// close query instance
+		query.close();
+
+		// close session
+		session.close();
+		
+	*/
+		var category = {name: "All", count : 10}
+		var categories = [category];
+		
+		res.render('explore', {isHomePage: false,
+								categories : categories});
     });
     
     
